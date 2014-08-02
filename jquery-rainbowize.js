7388ef880e4643cc7aa89e30df174b2603483ed5
Brett Schellenberg <brett.schellenberg@gmail.com>
2014-08-02 14:57:16 -0700

    Fixed handling of pure text nodes, various bugs

--- jquery-rainbowize.js
+++ jquery-rainbowize.js
@@ -9,11 +9,40 @@
 
 (function( $ ) {
 	$.fn.rainbowize = function() {
-		return $(this).each(function() {
+		//rainbowize helpers
+		function color_from_hue(hue) {
+			var h = hue/60;
+			var c = 255;
+			var x = (1 - Math.abs(h%2 - 1))*255;
+			var color;
 
-			if ($(this).contents().length == 1
-				&& $(this).contents()[0].nodeType == Node.TEXT_NODE
-				&& ! $(this).contents()[0].nodeValue.match(/^\s*$/)) {
+			var i = Math.floor(h);
+			if (i == 0) color = rgb_to_hex(c, x, 0);
+			else if (i == 1) color = rgb_to_hex(x, c, 0);
+			else if (i == 2) color = rgb_to_hex(0, c, x);
+			else if (i == 3) color = rgb_to_hex(0, x, c);
+			else if (i == 4) color = rgb_to_hex(x, 0, c);
+			else color = rgb_to_hex(c, 0, x);
+
+			return color;
+		}
+
+		function rgb_to_hex(red, green, blue) {
+			var h = ((red << 16) | (green << 8) | (blue)).toString(16);
+			// add the beginning zeros
+			while (h.length < 6) h = '0' + h;
+			return '#' + h;
+		}
+
+		// text nodes in the DOM wouldn't be able to accept styling, so convert those to element nodes (spans)
+		$(this).contents().filter(function() {
+			return this.nodeType == Node.TEXT_NODE && ! this.nodeValue.match(/^\s*$/);
+		}).wrap('<span class="rainbowizedTextNode"></span>');
+
+
+		// do the rainbowizing
+		return $(this).each(function() {
+			if (this.nodeType == Node.ELEMENT_NODE && $(this).contents().length == 1) {
 
 				var rainbowtext = '';
 				var hue=Math.floor((Math.random() * 360) + 1); //random starting color
@@ -21,49 +50,24 @@
 				var step=0;
 
 				// get the current text inside element, stripping leading and trailing whitespace
-				var text = $(this).text().replace(/^\s*/g,'').replace(/\s*$/g,'');
+				var text = this.textContent.replace(/^\s*/g,'').replace(/\s*$/g,'');
 
-				// hue is 360 degrees
-				if (text.length > 0)
+				// the total length of text should cover 360 degrees, so a step is 360/length
 				step = 360 / (text.length);
 
-				// iterate the whole 360 degrees
+				// wrap each letter in the text with a span of the appropriate color
 				for (var i = 0; i < text.length; i++) {
 					rainbowtext = rainbowtext + '<span style="color:' + color_from_hue(hue) + '">' + text.charAt(i) + '</span>';
 					hue += step;
-					if (hue >=360) { hue -= 360; }
+					if (hue >=360) { hue -= 360; } //wrap from 360 -> 0, like a color wheel
 				}
 
-				$(this).html(rainbowtext);
 				$(this).data('originaltext', text); //save original state for derainbowize()
+				$(this).html(rainbowtext);
 			}
 			else {
-				$(this).contents().rainbowize();
-			}
-
-			//rainbowize helpers
-			function color_from_hue(hue) {
-				var h = hue/60;
-				var c = 255;
-				var x = (1 - Math.abs(h%2 - 1))*255;
-				var color;
-
-				var i = Math.floor(h);
-				if (i == 0) color = rgb_to_hex(c, x, 0);
-				else if (i == 1) color = rgb_to_hex(x, c, 0);
-				else if (i == 2) color = rgb_to_hex(0, c, x);
-				else if (i == 3) color = rgb_to_hex(0, x, c);
-				else if (i == 4) color = rgb_to_hex(x, 0, c);
-				else color = rgb_to_hex(c, 0, x);
-
-				return color;
-			}
-
-			function rgb_to_hex(red, green, blue) {
-				var h = ((red << 16) | (green << 8) | (blue)).toString(16);
-				// add the beginning zeros
-				while (h.length < 6) h = '0' + h;
-				return '#' + h;
+				// recurse down a level
+				$(this).children().rainbowize();
 			}
 		});
 	};
@@ -72,6 +76,9 @@
 		return this.each(function() {
 			if ($(this).data('originaltext')) {
 				$(this).html($(this).data('originaltext'));
+
+				// unwrap text nodes to restore original state of page
+				$(this).filter('span.rainbowizedTextNode').contents().unwrap();
 			}
 			else {
 				$(this).contents().derainbowize();
